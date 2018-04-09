af72f35b984c7cbd372159ccead96eca21f453ad
anicete <marcioanicete@gmail.com>
2018-04-09 16:13:11 -0300

    teste1

---
+++ teste.c
@@ -0,0 +1,11 @@
+// Criado em				: 2018/04/09
+// Ult. Atualização : 2018/04/09
+// Autor			      : Marcio Anicete <marcioanicete@gmail.com>
+// Notes      			: Descrever sobre o Programa 
+// Compilação				: gcc 
+// Execução					: ./a.out
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
